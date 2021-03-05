61e56410385ed11c76bef4cea992e0bda487af28
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2021-03-05 21:26:28 +0100

    Delete signin.js

--- signin.js
+++
@@ -1,9 +0,0 @@
-var forgot = document.getElementById("fgt");
-forgot.addEventListener("mousedown", function(){
-	forgot.style.boxShadow="4px 4px 5px black";
-	forgot.style.border="solid #0000ff33 3px";
-});
-forgot.addEventListener("mouseup", function(){
-	forgot.style.boxShadow="";
-	forgot.style.border="solid black 0px";
-});
