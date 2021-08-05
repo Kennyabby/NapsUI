12a229eb907e1353e29b38e9a5e2f2a4c96e2acc
Kennyabby <kennyabby22@gmail.com>
2021-08-05 19:55:14 +0100

    focussing on dashboard

--- views/dashboard.js
+++ views/dashboard.js
@@ -1,5 +1,16 @@
 
-var welcomeMessage = document.getElementById("welcome");
+var logSection=document.getElementById("welcome");
+var profile = document.getElementById("profile");
+var notifications = document.getElementById("notifications");
+var events = document.getElementById("events");
+var tasks = document.getElementById("tasks");
+var settings = document.getElementById("settings");
+var dashSection = document.getElementById("dashSection");
+var alluminiSection = document.getElementById("alluminiSection");
+var votingSection = document.getElementById("profile");
+var discussionSection = document.getElementById("profile");
+var adminSection = document.getElementById("profile");
+
 async function loadDashboard(){
 	try{
 
@@ -10,7 +21,7 @@ async function loadDashboard(){
 				'Content-Type': 'application/json'
 			}
 		};
-
+		
 		const response= await fetch("/NapsDetails", opts);
 		const User = await response.json();
 		const Napsite = await User.details;
@@ -18,9 +29,13 @@ async function loadDashboard(){
 		
 		await console.log(Napsite);
 
-		welcomeMessage.innerHTML=`Welcome to Naps Dashboard Dear ${Napsite.LastName} ${Napsite.FirstName} ${Napsite.MiddleName} with Matric No ${Napsite.MatricNo}`;
+		var spanTag = document.createElement("span");
+		// spanTag.appendChild(document.createTextNode(`${Napsite.LastName} ${Napsite.FirstName} ${Napsite.MiddleName}`));
+		spanTag.appendChild(document.createTextNode(`${Napsite.UserName}`));
+		spanTag.style.fontWeight="bold";
+		logSection.appendChild(spanTag);
 	}catch(TypeError){
-		
+
 	}
 	
 }
